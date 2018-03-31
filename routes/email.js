e2b985c17d806b6b511b578d82138defcc1d1865
vicroni <drasa_tec@hotmail.com>
2018-03-31 15:48:23 -0600

    Confirmar email

--- routes/email.js
+++ routes/email.js
@@ -4,30 +4,33 @@ const rsa = require('./../security/rsa/rsa');
 // email sender function
 exports.sendEmail = function (request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-    const {email, id_usuario}  = request.body;
+    const { email, id_usuario } = request.body;
     let firma = SHA256("Como estas?" + email + "Yo jaiba y tu?" + id_usuario).toString();
     let cifrado = rsa.cifrar(firma, 41, 309);
     // Definimos el transporter
-    const transporter = nodemailer.createTransport({
-        service: 'Hotmail',
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true, // use SSL
         auth: {
-            user: 'drasa_tec@hotmail.com',
-            pass: 'Cometa204'
+            user: 'info@bombesoftware.com',
+            pass: 'Hola1234'
         }
     });
+
     // Definimos el email
     const mailOptions = {
-        from: 'Bombe sofwtare',
+        from: '"Bombe software " <info@bombesoftware.com>',
         to: email,
         subject: 'Confirmacion de correo',
-        text: "Saludos, tu codigo de acceso es: " + cifrado
+        html: `<p>Tu codigo es: <b>${cifrado}</b></p>`
     };
     // Enviamos el email
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            response.write(JSON.stringify({message: error.message}));
+            console.log(JSON.stringify({ message: error.message }));
         } else {
-            response.write(JSON.stringify({message: "Todo"}));
+            console.log(JSON.stringify({ message: "Todo" }));
         }
         response.end();
     });
