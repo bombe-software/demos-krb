4f0e04219e6fb85d42e51d3681baf2d68d20210b
vicroni <drasa_tec@hotmail.com>
2018-04-07 22:12:32 -0500

    Deploy rapido

--- deploy.js
+++ deploy.js
@@ -6,7 +6,8 @@ exports.demos_web_http = 'https://www.demos-web.com';
 exports.demos_krb_http = "https://demos-krb.herokuapp.com";
 exports.demos_gql_http = "https://demos-gql.herokuapp.com";
 exports.demos_gql_ws = "wss://demos-gql.herokuapp.com";
-exports.validation_http = true;
+exports.deploy = true;
+exports.MONGO_URI = `mongodb://${admin}:${n0m3l0}@ds255767.mlab.com:55767/demos_db`;
  */
 
 
@@ -17,4 +18,5 @@ exports.demos_web_http = "http://localhost:9000";
 exports.demos_krb_http = "http://localhost:5000";
 exports.demos_gql_http = "http://localhost:3000";
 exports.demos_gql_ws = "ws://localhost:3000";
-exports.validation_http = false;
+exports.deploy = false;
+exports.MONGO_URI = `mongodb://localhost/demos_db`;
