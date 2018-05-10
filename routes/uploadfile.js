467171228ec0da261156b166af585693cfc62ef2
MedinaVilla <diletalvezalaautopista@gmail.com>
2018-05-09 22:31:06 -0500

    S

--- routes/uploadfile.js
+++ routes/uploadfile.js
@@ -2,7 +2,7 @@ const fileUpload = require('express-fileupload');
 var path = require('path')
 exports.Uploads = function(req, res) {
     let file = req.files.file;
-    let filenameIdUsuario = req.body.idUsuario
+    let filenameIdUsuario = req.body.id_solicitud
     // Use the mv() method to place the file somewhere on your server
     let ext = path.extname(file.name);
     if((ext === ".jpg")||(ext === ".jpeg")||(ext === ".png")){
