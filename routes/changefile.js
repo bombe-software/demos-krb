a30dec6c1f9231076890a248d3c8feb839a3b9fc
vicroni <drasa_tec@hotmail.com>
2018-05-17 22:53:48 -0500

    Cambios de ruta

--- routes/changefile.js
+++ routes/changefile.js
@@ -4,7 +4,7 @@ var fs = require('file-system');
 exports.ChangeRoute = function(req, res) {
     let newFileName = req.body.fileName;
     let oldFileName = req.body.oldFileName;
-    fs.rename(oldFileName, newFileName, function (err) {
+    fs.rename('/public/img/'+oldFileName, '/public/img/'+newFileName, function (err) {
       if (err) throw err;
       console.log('renamed complete');
     });
